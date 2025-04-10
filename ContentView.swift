69b18a3c3fb7604750308fdf732640bd92e2fe93
Aadi Shha <ashah8399@stu.d214.org>
2025-04-10 08:50:52 -0700

    made var

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number1 : Int?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+
+        
     }
 }
