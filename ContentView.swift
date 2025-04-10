519962dfca811f3aa79ed284673b53e02b1082c4
Karima Beisheeva <beisheevakarima@icloud.com>
2025-04-10 10:57:50 -0500

    I created TextField and connected it to var number1

--- ContentView.swift
+++ ContentView.swift
@@ -4,7 +4,7 @@ struct ContentView: View {
     @State var number1 : Int?
     @State var number2: Int?
     var body: some View {
-
+        TextField("Enter Number", value:$number1, format: .number)
         
     }
 }
