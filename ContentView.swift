b26c7045dd377888b1320390ee8d66d43a00c6a3
Aadi Shha <ashah8399@stu.d214.org>
2025-04-10 09:01:49 -0700

    made answer var and displayed it

--- ContentView.swift
+++ ContentView.swift
@@ -3,9 +3,10 @@ import SwiftUI
 struct ContentView: View {
     @State var number1 : Int?
     @State var number2: Int?
+    @State var answer = 0
     var body: some View {
         TextField("Enter Number", value:$number1, format: .number)
-        
         TextField("Enter Number", value: $number2, format: .number)
+        Text("\(answer)")
     }
 }
